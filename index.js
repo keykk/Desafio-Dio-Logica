40cc71d65974e59c69827dc526b958109b9ebb66
keykk <keykkashi@gmail.com>
2024-05-01 09:59:59 -0300

    Heroi criado

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
-console.log("teste");
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+function determinarNivelXP(xp) {
+    switch (true) {
+      case xp <= 1000:
+        return "Ferro";
+      case xp >= 1001 && xp <= 2000:
+        return "Bronze";
+      case xp >= 2001 && xp <= 5000:
+        return "Prata";
+      case xp >= 5001 && xp <= 7000:
+        return "Ouro";
+      case xp >= 7001 && xp <= 8000:
+        return "Platina";
+      case xp >= 8001 && xp <= 9000:
+        return "Ascendente";
+      case xp >= 9001 && xp <= 10000:
+        return "Imortal";
+      case xp >= 10001:
+        return "Radiante";
+      default:
+        return "Nível não determinado";
+    }
+  }
+
+
+// Função recursiva para solicitar entrada do usuário até que ele digite "sair"
+let texto = "Digite o nome do Heroi ";
+let nome = "";
+function getUserInput() { 
+  rl.question(texto+'(digite "sair" para encerrar): ', (userInput) => {
+    if (userInput.toLowerCase() === 'sair') {
+      rl.close(); // Encerra a leitura do input
+    } else  if (!isNaN(userInput)) {
+        let xp = parseFloat(userInput);
+        console.log("O Herói de nome "+nome+" está no nível de "+determinarNivelXP(xp));
+        rl.close(); 
+    } else {
+      //console.log('Bem vindo Heroi ', userInput);
+      nome = userInput;
+      texto = "Digite a experiencia do heroi ";
+      getUserInput(); // Chama a função novamente para solicitar mais entrada
+    }
+  });
+}
+
+// Inicia o processo solicitando a primeira entrada do usuário
+getUserInput();
